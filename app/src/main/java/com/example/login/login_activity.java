e56f6033bbf2eb0347963a6b56099dbcaaed4d73
jaehyeon2 <59330378+jaehyeon2@users.noreply.github.com>
2020-01-02 00:33:08 +0900

    login_activity에 toast 추가

--- app/src/main/java/com/example/login/login_activity.java
+++ app/src/main/java/com/example/login/login_activity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class login_activity extends AppCompatActivity {
 
@@ -15,15 +16,27 @@ public class login_activity extends AppCompatActivity {
 
         final Button login_btn = findViewById(R.id.login);
         Button signup_btn = findViewById(R.id.signup);
-        EditText login_id_ = findViewById(R.id.login_ID);
-        EditText login_pw_ = findViewById(R.id.login_PW);
+        final EditText login_id_ = findViewById(R.id.login_ID);
+        final EditText login_pw_ = findViewById(R.id.login_PW);
 
         login_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent login_ = new Intent(login_activity.this, app_main.class);
-                startActivity(login_);
-                finish();
+
+                if(login_id_.length()==0) {
+                    Toast.makeText(getApplicationContext(), "ID를 입력해주세요", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                else if(login_pw_.length()==0) {
+                    Toast.makeText(getApplicationContext(), "PW를 입력해주세요", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                else{
+                    Intent login_ = new Intent(login_activity.this, app_main.class);
+                    startActivity(login_);
+                    finish();
+                    Toast.makeText(getApplicationContext(), "로그인 되었습니다.", Toast.LENGTH_SHORT).show();
+                }
             }
         });
 
