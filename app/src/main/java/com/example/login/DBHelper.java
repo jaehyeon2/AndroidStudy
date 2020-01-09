1022fec36ca506213915f4cff3a54943008e3150
jaehyeon2 <59330378+jaehyeon2@users.noreply.github.com>
2020-01-09 21:27:19 +0900

    DB_insert 함수 추가, main에 적용

--- app/src/main/java/com/example/login/DBHelper.java
+++ app/src/main/java/com/example/login/DBHelper.java
@@ -8,9 +8,15 @@ import android.view.View;
 public class DBHelper extends SQLiteOpenHelper {
 
     public static final int DATABASE_VERSION = 1;
-    public DBHelper(View.OnClickListener context) {
-        super((Context) context, "USER_TABLE", null, DATABASE_VERSION);
+
+    public DBHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int DATABASE_VERSION) {
+        super(context, name, factory, DATABASE_VERSION);
     }
+    /*public DBHelper(Context applicationContext, String user_table, View.OnClickListener context, int i) {
+        super((Context) context, "USER_TABLE", null, DATABASE_VERSION);
+    }*/
+
+
 
     @Override
     public void onCreate(SQLiteDatabase db) {
@@ -33,7 +39,7 @@ public class DBHelper extends SQLiteOpenHelper {
         db.execSQL("INSERT INTO USER_INFO VALUES(null, '" + user_ID + "', " + user_PW + ", '" + Name + "');");
         db.close();
     }
-    public void update(String Name, String user_ID, String user_PW) {
+    /*public void update(String Name, String user_ID, String user_PW) {
 
-    }
+    }*/
 }
--- app/src/main/java/com/example/login/signup_activity.java
+++ app/src/main/java/com/example/login/signup_activity.java
@@ -2,12 +2,16 @@ package com.example.login;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import static com.example.login.DBHelper.DATABASE_VERSION;
 
 public class signup_activity extends AppCompatActivity {
 
@@ -15,11 +19,18 @@ public class signup_activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.signup);
 
+        final DBHelper dbHelper = new DBHelper(getApplicationContext(), "USER_TABLE", null, DATABASE_VERSION);
+
         final Button fin_signup_btn = findViewById(R.id.signup_fin);
         final EditText signup_name_ = findViewById(R.id.signup_Name);
         final EditText signup_id_ = findViewById(R.id.signup_ID);
         final EditText signup_pw_ = findViewById(R.id.signup_PW);
         final EditText signup_pw2_ = findViewById(R.id.signup_PW2);
+
+        final String name = signup_name_.getText().toString();
+        final String ID = signup_id_.getText().toString();
+        final String PW = signup_pw_.getText().toString();
+        final String PW2 = signup_pw2_.getText().toString();
 
         fin_signup_btn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -40,7 +51,10 @@ public class signup_activity extends AppCompatActivity {
                     Toast.makeText(getApplicationContext(), "PW confirm을 입력해주세요", Toast.LENGTH_SHORT).show();
                     return;
                 }
-                else if(signup_pw_.getText().toString().equals(signup_pw2_.getText().toString())) {
+                else if(PW.equals(PW2)) {
+
+                    dbHelper.insert(name, ID, PW);
+
                     Intent signup_fin_ = new Intent(signup_activity.this, login_activity.class);
                     startActivity(signup_fin_);
                     finish();
