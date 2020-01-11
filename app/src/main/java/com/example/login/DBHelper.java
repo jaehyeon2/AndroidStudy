a7542f8e0c5d58afd22454fea804a2967fde8174
jaehyeon2 <59330378+jaehyeon2@users.noreply.github.com>
2020-01-11 17:46:22 +0900

    Cursor_Adapter/list 추가 보류,,,(어려움) 추후 공부 후 다시 해보기

--- app/src/main/java/com/example/login/DBHelper.java
+++ app/src/main/java/com/example/login/DBHelper.java
@@ -1,6 +1,7 @@
 package com.example.login;
 
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.view.View;
@@ -8,6 +9,8 @@ import android.view.View;
 public class DBHelper extends SQLiteOpenHelper {
 
     public static final int DATABASE_VERSION = 1;
+
+    DBHelper mydb;
 
     public DBHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int DATABASE_VERSION) {
         super(context, name, factory, DATABASE_VERSION);
@@ -36,9 +39,12 @@ public class DBHelper extends SQLiteOpenHelper {
     public void insert(String Name, String user_ID, String user_PW) {
         SQLiteDatabase db = getWritableDatabase();
 
+        //Cursor c = db.rawQuery("SELECT name FROM USER_TABLE WHERE type='table'", null);
+
         db.execSQL("INSERT INTO USER_INFO VALUES(null, '" + user_ID + "', " + user_PW + ", '" + Name + "');");
         db.close();
     }
+
     /*public void update(String Name, String user_ID, String user_PW) {
 
     }*/
