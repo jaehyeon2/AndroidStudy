624cd10d438d01fb00d035b33c83a6a098b198b5
jaehyeon2 <59330378+jaehyeon2@users.noreply.github.com>
2020-01-13 22:23:22 +0900

    완성?형태이지만 결과 버튼을 눌렀을 때 동작 실행이 되지 않음

--- app/src/main/java/com/example/calculator_button/MainActivity.java
+++ app/src/main/java/com/example/calculator_button/MainActivity.java
@@ -12,16 +12,8 @@ import java.lang.reflect.Array;
 
 public class MainActivity extends AppCompatActivity {
 
-    //사칙연산 파악을 위한 type
-    int type;
-    int PLUS = 0, MINUS = 1, MULTIPLY = 2, DIVIDE = 3;
-    String history="";
-    String number1="";
-    String number2="";
-    double d1,d2;
-
-
-
+    int a;
+    int where = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,258 +33,112 @@ public class MainActivity extends AppCompatActivity {
 
         final EditText progress = findViewById(R.id.progress);
         final EditText result = findViewById(R.id.result);
-        final Button equal= findViewById(R.id.equals);
+        final Button equal= findViewById(R.id.equal);
 
         final Button plus = findViewById(R.id.plus);
         final Button minus = findViewById(R.id.minus);
         final Button multiply = findViewById(R.id.multiply);
         final Button divide = findViewById(R.id.divide);
 
-        final Button delete = findViewById(R.id.delete);
-        final Button clear = findViewById(R.id.clear);
 
-        //Clear Button
-        clear.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                progress.setText("");
-                result.setText("");
-            }
-        });
+        final Button clear = findViewById(R.id.Clear);
 
-        //Number Button (0~9)
-        num0.setOnClickListener(new View.OnClickListener() {
+        View.OnClickListener btn_click = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                progress.setText(progress.getText().toString() + 0);
-            }
-        });
-        num1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                progress.setText(progress.getText().toString() + 1);
-            }
-        });
-        num2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                progress.setText(progress.getText().toString() + 2);
-            }
-        });
-        num3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                progress.setText(progress.getText().toString() + 3);
-            }
-        });
-        num4.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                progress.setText(progress.getText().toString() + 4);
-            }
-        });
-        num5.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                progress.setText(progress.getText().toString() + 5);
-            }
-        });
-        num6.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                progress.setText(progress.getText().toString() + 6);
-            }
-        });
-        num7.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                progress.setText(progress.getText().toString() + 7);
-            }
-        });
-        num8.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                progress.setText(progress.getText().toString() + 8);
-            }
-        });
-        num9.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                progress.setText(progress.getText().toString() + 9);
-            }
-        });
-
-        plus.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-            }
-        });
-
-        /*Button.OnClickListener mListener = new Button.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (progress == null){
-                    Toast.makeText(MainActivity.this, "수를 입력하세요", Toast.LENGTH_LONG).show();
-                    return;
+                if(v==num0){
+                    progress.setText(progress.getText().toString()+0);
                 }
-                switch (v.getId()) {
-                    case R.id.plus:
-                        number1 = result.getText().toString();
-                        history = result.getText().toString()+" + ";
-                        progress.setText(history);
-                        result.setText("");
-
-                        type = PLUS;
-                        break;
-
-                    case R.id.minus:
-                        number1 = result.getText().toString();
-                        history = result.getText().toString()+" - ";
-                        progress.setText(history);
-                        result.setText("");
-
-                        type = MINUS;
-                        break;
-
-                    case R.id.multiply:
-                        number1 = result.getText().toString();
-                        history = result.getText().toString()+" × ";
-                        progress.setText(history);
-                        result.setText("");
-
-                        type = MULTIPLY;
-                        break;
-
-                    case R.id.divide:
-                        number1 = result.getText().toString();
-                        history = result.getText().toString()+" ÷ ";
-                        progress.setText(history);
-                        result.setText("");
-
-                        type = DIVIDE;
-                        break;
-
-                    case R.id.delete:
-                        String del = result.getText().toString();
-                        Toast.makeText(MainActivity.this, del, Toast.LENGTH_SHORT).show();
-                        result.setText(del.substring(0,del.length() - 1));
-                        break;
-
-                    case R.id.result :
-                        double db_result = 0;
-                        Toast.makeText(MainActivity.this, "결과", Toast.LENGTH_SHORT).show();
-                        number2 = result.getText().toString();
-                        history = history + result.getText().toString();
-                        progress.setText(history);
-
-                        d1 = Double.parseDouble(number1);
-                        d2 = Double.parseDouble(number2);
-
-                        if(type == PLUS) {
-                            db_result = d1 + d2;
-                            result.setText("" + db_result);
-                        } else if (type == MINUS) {
-                            db_result = d1 - d2;
-                            result.setText("" + db_result);
-                        }else if (type == MULTIPLY) {
-                            db_result = d1 * d2;
-                            result.setText("" + db_result);
-                        }else if (type == DIVIDE) {
-                            db_result = d1 / d2;
-                            result.setText("" + db_result);
-                        }else if (type == REMAINDER) {
-                            result = d1 % d2;
-                            et_result.setText("" + db_result);
-                        }
-
-                       `number1 = result.getText().toString();
-                        result.setText(number1);
-                        break;
-
-                    case R.id.num0 :
-                        result.setText(result.getText().toString() + 0); break;
-                    case R.id.num1 :
-                        result.setText(result.getText().toString() + 1); break;
-                    case R.id.num2 :
-                        result.setText(result.getText().toString() + 2); break;
-                    case R.id.num3 :
-                        result.setText(result.getText().toString() + 3); break;
-                    case R.id.num4 :
-                        result.setText(result.getText().toString() + 4); break;
-                    case R.id.num5 :
-                        result.setText(result.getText().toString() + 5); break;
-                    case R.id.num6 :
-                        result.setText(result.getText().toString() + 6); break;
-                    case R.id.num7 :
-                        result.setText(result.getText().toString() + 7); break;
-                    case R.id.num8 :
-                        result.setText(result.getText().toString() + 8); break;
-                    case R.id.num9 :
-                        result.setText(result.getText().toString() + 9); break;
-                    case R.id.point :
-                        result.setText(result.getText().toString() + "."); break;
+                else if(v==num1){
+                    progress.setText(progress.getText().toString()+1);
                 }
-            }
-        };*/
-
-       /* Button.OnClickListener number = new Button.OnClickListener(){
-            @Override
-            public void onClick(View v) {
-                switch (v.getId()){
-                    case R.id.num0 :
-                        result.setText(result.getText().toString() + 0); break;
-                    case R.id.num1 :
-                        result.setText(result.getText().toString() + 1); break;
-                    case R.id.num2 :
-                        result.setText(result.getText().toString() + 2); break;
-                    case R.id.num3 :
-                        result.setText(result.getText().toString() + 3); break;
-                    case R.id.num4 :
-                        result.setText(result.getText().toString() + 4); break;
-                    case R.id.num5 :
-                        result.setText(result.getText().toString() + 5); break;
-                    case R.id.num6 :
-                        result.setText(result.getText().toString() + 6); break;
-                    case R.id.num7 :
-                        result.setText(result.getText().toString() + 7); break;
-                    case R.id.num8 :
-                        result.setText(result.getText().toString() + 8); break;
-                    case R.id.num9 :
-                        result.setText(result.getText().toString() + 9); break;
-                    case R.id.point :
-                        result.setText(result.getText().toString() + "."); break;
+                else if(v==num2){
+                    progress.setText(progress.getText().toString()+2);
+                }
+                else if(v==num3){
+                    progress.setText(progress.getText().toString()+3);
+                }
+                else if(v==num4){
+                    progress.setText(progress.getText().toString()+4);
+                }
+                else if(v==num5){
+                    progress.setText(progress.getText().toString()+5);
+                }
+                else if(v==num6){
+                    progress.setText(progress.getText().toString()+6);
+                }
+                else if(v==num7){
+                    progress.setText(progress.getText().toString()+7);
+                }
+                else if(v==num8){
+                    progress.setText(progress.getText().toString()+8);
+                }
+                else if(v==num9){
+                    progress.setText(progress.getText().toString()+9);
                 }
 
-            }
+                else if(v==plus){
+                    a=Integer.valueOf(progress.getText().toString().trim());
+                    progress.setText("");
+                    where=1;
+                }
+                else if(v==minus){
+                    a=Integer.valueOf(progress.getText().toString().trim());
+                    progress.setText("");
+                    where=2;
+                }
+                else if(v==multiply){
+                    a=Integer.valueOf(progress.getText().toString().trim());
+                    progress.setText("");
+                    where=3;
+                }
+                else if(v==divide){
+                    a=Integer.valueOf(progress.getText().toString().trim());
+                    progress.setText("");
+                    where=4;
+                }
 
+                else if(v==result) {
+                    if (where == 1) {
+                        a = a + Integer.valueOf(progress.getText().toString());
+                        progress.setText(Integer.toString(a));
+                    }
+                    else if(where==2){
+                        a = a - Integer.valueOf(progress.getText().toString());
+                        progress.setText(Integer.toString(a));
+                    }
+                    else if(where==3){
+                        a = a * Integer.valueOf(progress.getText().toString());
+                        progress.setText(Integer.toString(a));
+                    }
+                    else if(where==4){
+                        a = a / Integer.valueOf(progress.getText().toString());
+                        progress.setText(Integer.toString(a));
+                    }
+                }
+                else if(v==clear){
+                    progress.setText("");
+                    a=0;
+                }
+            }
         };
 
-
-        num0.setOnClickListener(number);
-        num1.setOnClickListener(number);
-        num2.setOnClickListener(number);
-        num3.setOnClickListener(number);
-        num4.setOnClickListener(number);
-        num5.setOnClickListener(number);
-        num6.setOnClickListener(number);
-        num7.setOnClickListener(number);
-        num8.setOnClickListener(number);
-        num9.setOnClickListener(number);
-
-        plus.setOnClickListener(mListener);
-        minus.setOnClickListener(mListener);
-        multiply.setOnClickListener(mListener);
-        divide.setOnClickListener(mListener);
-        remainder.setOnClickListener(mListener);
-        result.setOnClickListener(mListener);
-        delete.setOnClickListener(mListener);
-        */
-
-
-
-
-
+        num0.setOnClickListener(btn_click);
+        num1.setOnClickListener(btn_click);
+        num2.setOnClickListener(btn_click);
+        num3.setOnClickListener(btn_click);
+        num4.setOnClickListener(btn_click);
+        num5.setOnClickListener(btn_click);
+        num6.setOnClickListener(btn_click);
+        num7.setOnClickListener(btn_click);
+        num8.setOnClickListener(btn_click);
+        num9.setOnClickListener(btn_click);
+        plus.setOnClickListener(btn_click);
+        minus.setOnClickListener(btn_click);
+        multiply.setOnClickListener(btn_click);
+        divide.setOnClickListener(btn_click);
+        clear.setOnClickListener(btn_click);
+        equal.setOnClickListener(btn_click);
 
     }
 }
