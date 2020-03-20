58fbfe0324f4033f1f168df336e861ce64389ab2
Kang JaeHyeon <59330378+jaehyeon2@users.noreply.github.com>
2020-03-20 20:26:04 +0900

    기본 틀

--- RecyclerView_practice1/app/src/main/java/com/example/recyclerview_practice1/MainActivity.java
+++ RecyclerView_practice1/app/src/main/java/com/example/recyclerview_practice1/MainActivity.java
@@ -1,14 +1,52 @@
 package com.example.recyclerview_practice1;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    //Button addbtn = findViewById(R.id.add_btn);
+    //Button dltbtn = findViewById(R.id.dlt_btn);
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        //데이터 리스트 생성
+        ArrayList<String> list = new ArrayList<>();
+        for(int i=0; i<10; i++) {
+            list.add(String.format("TEXT %d", i+1));
+        }
+
+        /*addbtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                num++;
+            }
+        });
+
+        dltbtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                num--;
+            }
+        });*/
+
+
+        //LinearLayoutManager 객체 지정
+        RecyclerView recyclerView = findViewById(R.id.recyclerview);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        RecyclerView_Adapter adapter = new RecyclerView_Adapter(list);
+        recyclerView.setAdapter(adapter);
     }
 }
